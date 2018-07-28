1e6e0d1081a242f006f10191fafff6474a87650b
CourtneyDL <courtneyleacock@googlemail.com>
2018-07-28 11:31:38 +0100

    Test 1, AC3&4 - Screenreader support for input, focus on component when PickUpInput mounts

--- source/js/components/PickUp/PickUpInput.jsx
+++ source/js/components/PickUp/PickUpInput.jsx
@@ -12,17 +12,33 @@ export default class PickUpInput extends Component {
         onChange: () => {}
     }
 
+    focusInput () {
+        if (this.input_elem) {
+            this.input_elem.focus();
+        }
+    }
+
     onChange(e) {
         this.props.onChange(e.currentTarget.value);
+    }
+
+    componentDidMount () {
+        this.focusInput();
     }
 
     render() {
         return (
             <div className="pickup-location-input">
-                <label htmlFor="search_term">Pick-Up Location</label>
-                <input type="text" name="search_term" value={this.props.search_term} 
-                    placeholder="city, airport, station, region and district..."
-                    onChange={this.onChange.bind(this)}/>
+                <fieldset>
+                    <label id="search_term_label" htmlFor="search_term">Pick-Up Location</label>
+                    <input type="text" id="search_term" name="search_term" value={this.props.search_term} 
+                        ref={elem => this.input_elem = elem}
+                        placeholder="city, airport, station, region and district..."
+                        aria-autocomplete="list" aria-haspopup="true"
+                        aria-required="true"
+                        aria-describedby="search_term_description"
+                        onChange={this.onChange.bind(this)}/>
+                </fieldset>
             </div>
         );
     }
--- test/components/app/PickUpInput.spec.js
+++ test/components/app/PickUpInput.spec.js
@@ -6,14 +6,22 @@ import PickUpInput from '../../../source/js/components/PickUp/PickUpInput';
 
 describe('components/PickUp/PickUpInput', function () {
     it('should be in its base state', function () {
-        const wrapper = shallow(<PickUpInput/>);
+        const wrapper = mount(<PickUpInput/>);
 
         expect(wrapper.find('div.pickup-location-input')).to.have.length(1,'Root element class missing')
-        expect(wrapper.contains(<label htmlFor="search_term">Pick-Up Location</label>)).to.equal(true, 'Label missing');
+        expect(wrapper.contains(<label id="search_term_label" htmlFor="search_term">Pick-Up Location</label>)).to.equal(true, 'Label missing');
         
         const input_elem = wrapper.find('input[name="search_term"]');
         expect(input_elem).to.have.length(1, 'Input missing');
-        expect(input_elem.prop('placeholder')).to.equal('city, airport, station, region and district...');
+        expect(input_elem.prop('id')).to.equal('search_term', 'input id missing');
+        expect(input_elem.prop('placeholder')).to.equal('city, airport, station, region and district...', 'placeholder missing');
+                
+        expect(input_elem.prop('aria-autocomplete')).to.equal('list', 'aria-autocomplete missing');
+        expect(input_elem.prop('aria-haspopup')).to.equal('true', 'aria-haspopup missing');
+        expect(input_elem.prop('aria-required')).to.equal('true', 'aria-required missing');
+
+        const focused_element = document.activeElement;
+        expect(focused_element.matches('input[name="search_term"]')).to.equal(true, 'input is not in focus');
     });
 
     it('should have a search term', function () {
