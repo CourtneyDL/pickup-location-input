fca1eacba9eb720ddf8f974f7808ff61a7998201
CourtneyDL <courtneyleacock@googlemail.com>
2018-08-10 20:08:57 +0100

    PickUpReact - A view with a purely react based solution to the test

--- source/js/views/App/index.jsx
+++ source/js/views/App/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import PickUp from 'views/PickUp';
+import PickUpReact from 'views/PickUpReact';
 
 const App = ({ results }) => (
     <div className="container">
         <PickUp/>
+        <PickUpReact/>
     </div>
 );
 
